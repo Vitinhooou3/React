11eb4c126c8f21aa79afa5de59ffe2b44a8fc939
Vitinhooo <barbosavictor739@gmail.com>
2024-02-24 23:01:01 -0300

    inicio do aprendizado

---
+++ src/Button.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+function Mybutton(){
+
+    const [showDiv, setShowDiv] = useState(false);
+
+    const toggleDiv = () => {
+        setShowDiv(!showDiv)
+    }
+    return(
+
+        <div>
+            <button onClick={toggleDiv}>Olá</button>
+            {showDiv && <div>Divfoda</div>}
+        </div>
+
+    );
+}
+
+export default Mybutton;
